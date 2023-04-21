5176065ef6ee45922b13e4612f51dd165235e91a
Hesh-Kavinda <heshan@ascnetic.se>
2023-04-21 12:11:57 +0530

    styled-component used for styling

--- src/components/CategoryView.tsx
+++ src/components/CategoryView.tsx
@@ -1,6 +1,6 @@
 import { CategoryModel } from "../interfaces/CategoryModel";
-import { Button } from "react-bootstrap";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import styled from 'styled-components';
 
 interface CategoryProp {
     data : CategoryModel[],
@@ -9,6 +9,27 @@ interface CategoryProp {
     valueChanged: (id : string, value: string) => void
 }
 
+const DeleteButton = styled.button`
+    font-size: 1em;
+    margin: 1em;
+    margin-left:0em;
+    padding: 0.25em 1em;
+    border: 2px solid tomato;
+    border-radius: 3px;
+    background-color: white;
+    color: tomato;
+`;
+
+const AddButton = styled.button`
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border: 2px solid mediumseagreen;
+    border-radius: 3px;
+    background-color: white;
+    color: mediumseagreen;
+`;
+
 function CategoryView({data, handleDelete, handleAdd, valueChanged}: CategoryProp){
 
     return <div>
@@ -16,8 +37,8 @@ function CategoryView({data, handleDelete, handleAdd, valueChanged}: CategoryPro
             data.map((item) => <div key={item.id}>
                 <li>
                     <input style={{borderStyle:'dashed'}} value={item.value} onChange={(event) => {valueChanged(item.id, event.target.value)}}/>
-                    <Button variant="primary" style={{margin:10}} onClick={() => {handleAdd(item.id)}}>add</Button>
-                    <Button variant="danger" style={{margin:10, marginLeft:0}} onClick={() => {handleDelete(item.id)}}>delete</Button>
+                    <AddButton onClick={() => {handleAdd(item.id)}}>Add</AddButton>
+                    <DeleteButton onClick={() => {handleDelete(item.id)}}>Delete</DeleteButton>
                 </li>
                 <div style={{paddingLeft:30}}><CategoryView data={item.children} handleDelete={handleDelete} handleAdd={handleAdd} valueChanged={valueChanged}/></div>
             </div>)
