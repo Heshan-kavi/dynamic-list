9ed98674bfb1fdee62c51f22e86ab182f69c3f47
Hesh-Kavinda <heshan@ascnetic.se>
2023-05-06 10:58:49 +0530

    Created a common button, used it on other buttons

--- src/components/CategoryView/CategoryView.styles.ts
+++ src/components/CategoryView/CategoryView.styles.ts
@@ -1,22 +1,24 @@
 import styled from "styled-components";
 
-export const DeleteButton = styled.button`
+interface PrimaryButtonInterface {
+    isDeleteButton : boolean;
+}
+
+export const PrimaryButton = styled.button`
     font-size: 1em;
     margin: 1em;
-    margin-left:0em;
     padding: 0.25em 1em;
     border-radius: 3px;
     background-color: white;
+`;
+
+export const DeleteButton = styled(PrimaryButton)`
+    margin-left:0em;
     color: tomato; 
     border: 2px solid tomato;
 `;
 
-export const AddButton = styled.button`
-    font-size: 1em;
-    margin: 1em;
-    padding: 0.25em 1em;
-    border-radius: 3px;
-    background-color: white;
+export const AddButton = styled(PrimaryButton)`
     color: mediumseagreen;
     border: 2px solid mediumseagreen;
 `;
