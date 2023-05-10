f32fc280530c9689b3200e4e7268ef5dc33b5feb
Hesh-Kavinda <heshan@ascnetic.se>
2023-05-10 08:23:56 +0530

    implemented basic routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
 import './App.css';
 import SecondaryPage from './pages/SecondaryPage';
 import ThirdPage from './pages/ThirdPage';
 
 function App() {
   return (
-    // <SecondaryPage/>
-    <ThirdPage/>
+    <BrowserRouter>
+    <Routes>
+      <Route path="secondary-page" element={<SecondaryPage/>} />
+      <Route path="third-page" element={<ThirdPage/>} />
+      <Route path="*" element={<Navigate to='secondary-page'/>} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
