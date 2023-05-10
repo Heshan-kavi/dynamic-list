c3e17be288ee66a9995085b7068c2ccb618baafa
Hesh-Kavinda <heshan@ascnetic.se>
2023-05-10 09:36:25 +0530

    implemented nested routes

---
+++ src/pages/UserProfile/UserProfile.styles.ts
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const UserProfileHeader = styled.h2`
+    color : mediumseagreen;
+    margin: 1em;
+    padding: 0.25em 1em;
+`
+
+export const PrimaryButton = styled.button`
+    font-size: 1em;
+    margin: 1em;
+    padding: 0.25em 1em;
+    border-radius: 3px;
+    background-color: white;
+    color: palevioletred;
+    border: 2px solid palevioletred;
+`
